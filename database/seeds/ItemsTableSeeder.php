69968192cf0da5729900ec382ef47525dd28649d
Niles Rowland <neterslandreau@gmail.com>
2019-08-22 09:49:42 -0400

    saving items table seeder to repo.

---
+++ database/seeds/ItemsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Item;
+
+
+class ItemsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file = Storage::disk('local')->get('master_list_070219_1.csv');
+        $rows = array_map('str_getcsv', explode("\n", $file));
+//        dd($rows);
+        foreach ($rows as $r => $row) {
+            if ($r !== 0) {
+                $item = new Item([
+                    'name' => $row[3],
+                    'upc_code' => $row[0],
+                    'size' => $row[5],
+                    'quantity' => $row[6],
+                    'net_cost' => $row[15],
+                    'net_case' => $row[32],
+                ]);
+                $item->save();
+            }
+        }
+    }
+}
